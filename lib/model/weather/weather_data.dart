5141743377b0788591ac8619c8a146e6ac2efa24
mbr1993 <boburmannonov93@gmail.com>
2023-08-16 22:24:10 +0500

    weather data

--- lib/model/weather/weather_data.dart
+++ lib/model/weather/weather_data.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_weather_app/model/weather/weather_data_current.dart';
 
 class WeatherData {
-  final WeatherCurrentData;
+  final WeatherCurrentData current;
+
+  WeatherData(this.current);
 }
